cdf11c50d1c50a6f1de1fbea83e37b2ca964dc86
saiwfy <173861120@qq.com>
2017-03-04 10:39:01 +0800

    添加会员接口

--- src/MemberService.php
+++ src/MemberService.php
@@ -3,31 +3,18 @@ namespace HeadingCardJcrm;
 
 class MemberService extends Base{
 
-	const OPENCARD_API = "/jcrm-server-card/rest/card/openCard";
-	const QUERY_API = "/jcrm-server-card/rest/card/query";
+	const QUERY_API = "/jcrm-server-card/rest/mbr/query";
 
 	function __construct()
 	{	
 		parent::__construct();
 	}
 
-	/**
-	 * 外部会员开卡
-	 */
 	
-	public function openCard($request)
-	{
-		$post_data['operCtx'] = getOperCtx();
-		$post_data['request'] = $request;
-		$post = json_encode($post_data);
-		$url = $this->global_config['domain'].self::OPENCARD_API;
-		$result = $this->http_client->post($url,$post)->getBody();
-		return($result);
-	}
-
 	/**
-	 * 获取卡状态
+	 * 通过会员手机获取会员信息
 	 */
+
 	public function query($mobile){
 		$post_data['conditions'] = [
 			[
